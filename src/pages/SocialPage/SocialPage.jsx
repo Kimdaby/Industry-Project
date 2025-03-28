121681e66fde39b92a1671397dee4f5411f41a83
DiscoSid89 <redbaron_575@hotmail.com>
2025-03-28 02:24:08 -0400

    add base page structure

---
+++ src/pages/SocialPage/SocialPage.jsx
@@ -0,0 +1,21 @@
+function SocialPage() {
+
+
+    return (
+        <>
+            <section className='social-page__headline'>
+                <h1>Hi User!</h1>
+                <h2>Your Friends</h2>
+            </section>
+            <section className='social-page__friends'>
+                <div className="social-page__friends-container">
+                    <img src='/'></img>
+
+                </div>
+            </section>
+
+        </>
+    );
+}
+
+export default SocialPage;
