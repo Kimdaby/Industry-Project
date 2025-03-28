be62cf9793968bf06e11b8b448a15e447fd54cb1
David Kim <davidhkim22@gmail.com>
2025-03-28 11:04:13 -0500

    inital commit

--- src/pages/FriendPage/FriendPage.jsx
+++ src/pages/FriendPage/FriendPage.jsx
@@ -1,69 +1,37 @@
-import "./SocialPage.scss";
+import "./FriendPage.scss";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import { useNavigate } from "react-router-dom";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
-function SocialPage() {
+function FriendPage() {
+  const navigate = useNavigate();
   return (
     <>
-      <Header />
       <div className="social-page">
-        <section className="social-page__headline">
-          <h1>Hi User!</h1>
-        </section>
-
-        <section className="social-page__searchbar">
-          <label className="social-page__searchbar-label"></label>
-          <input
-            className="social-page__searchbar-input"
-            placeholder="Find Friends"
-          ></input>
-          <img
-            src="/public/images/search-24px.svg"
-            alt="searchIcon"
-            className="social-page__seachbar-icon"
-          />
-        </section>
-
+        <Header />
+        <div className="back-button" onClick={() => navigate(-1)}>
+          <ArrowBackIosIcon fontSize="small" />
+        </div>
         <section className="social-page__friends">
-          <h2 className="social-page__friends-headline">My Network</h2>
           <div className="social-page__friends-container">
-            <div className="social-page__friends-bubble">
-              <img
-                src="/public/images/friend.jpg"
-                alt="phFriend 1"
-                className="social-page__friends-img"
-              />
-              <p>Zachary Smith</p>
-            </div>
-            <div className="social-page__friends-bubble">
-              <img
-                src="/public/images/friend.jpg"
-                alt="phFriend 2"
-                className="social-page__friends-img"
-              />
-              <p>Philomena Cunk</p>
-            </div>
-            <div className="social-page__friends-bubble">
-              <img
-                src="/public/images/friend.jpg"
-                alt="phFriend 3"
-                className="social-page__friends-img"
-              />
-              <p>Zachary Smith</p>
-            </div>
-            <div className="social-page__friends-bubble">
+            <div className="social-page__friends-bubble1">
               <img
                 src="/public/images/friend.jpg"
                 alt="phFriend 4"
                 className="social-page__friends-img"
               />
-              <p>Zachary Smith</p>
+              <p className="zach">Zachary Smith</p>
+              <p className="zachp">You share 3 mutuals in your network</p>
+              <p className="zachp">"Saxon should die already LOL"</p>
             </div>
           </div>
         </section>
         <section className="social-page__trending">
           <div className="social-page__trending-friends">
-            <h3 className="social-page__trending-headline">Friend Trends</h3>
+            <h3 className="social-page__trending-headline">
+              Keeping Up With Zachary
+            </h3>
             <div className="social-page__trending-friends-scroll">
               <article className="social-page__trending-card">
                 <img
@@ -106,7 +74,9 @@ function SocialPage() {
 
         <section className="social-page__trending">
           <div className="social-page__trending-friends">
-            <h3 className="social-page__trending-headline">Keep up Friends</h3>
+            <h3 className="social-page__trending-headline">
+              All-Time Favorites
+            </h3>
             <div className="social-page__trending-friends-scroll">
               <article className="social-page__trending-card">
                 <img
@@ -149,8 +119,8 @@ function SocialPage() {
 
         <section className="social-page__episode-section">
           <div className="social-page__episode-headline-container">
-            <h2 className="social-page__episode-headline">Your Circles</h2>
-            <p>+ see all</p>
+            <h2 className="social-page__episode-headline">Shared Circles</h2>
+            <p>+</p>
           </div>
           <div className="social-page__episode-card">
             <img
@@ -203,7 +173,7 @@ function SocialPage() {
         </section>
 
         <section className="social-page__episode-section">
-          <h2 className="social-page__episode-headline">Recommended Circle</h2>
+          <h2 className="social-page__episode-headline">Their Circles</h2>
           <div className="social-page__episode-card">
             <img
               className="social-page__episode-img"
@@ -221,11 +191,10 @@ function SocialPage() {
             />
           </div>
         </section>
+        <Footer />
       </div>
-
-      <Footer />
     </>
   );
 }
 
-export default SocialPage;
+export default FriendPage;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import FriendPage from "../FriendPage/FriendPage";
 
 function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+  return <FriendPage />;
 }
 
-export default HomePage
+export default HomePage;
