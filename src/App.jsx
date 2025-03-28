4a2f1908fd6e73f33b1784ef9d0124e5ebc060ed
joey shu <shu.joey@gmail.com>
2025-03-28 10:20:57 -0400

    create single-user-page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
 import SingleMoviePage from './pages/SingleMoviePage/SingleMoviePage';
 import SocialPage from "./pages/SocialPage/SocialPage";
+import SingleUserPage from "./pages/SingleUserPage/SingleUserPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path='/' element={<HomePage/>}/>
       <Route path='/tvshow' element={<SingleMoviePage/>}/>
       <Route path='/social' element={<SocialPage/>}/>
+      <Route path='/user1' element={<SingleUserPage/>}/>
     </Routes>
     </BrowserRouter>
   );
