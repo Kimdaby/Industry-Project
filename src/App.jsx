5f60e4672a64cffd1ae206fb2f8cd1be76ac3370
David Kim <davidhkim22@gmail.com>
2025-03-28 09:06:55 -0500

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import SingleMoviePage from "./pages/SingleMoviePage/SingleMoviePage";
+import HBOFooter from "./components/Footer/Footer";
+import HBOHeader from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <HBOHeader />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="tvshow" element={<SingleMoviePage />} />
+      </Routes>
+      <HBOFooter />
+    </BrowserRouter>
   );
 }
 
